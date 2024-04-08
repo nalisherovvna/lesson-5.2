22ede37315316f7ad860ec0dd6f0d493b1e0a0ff
nalisherovvna <mushknasiba@gmail.com>
2024-04-08 16:56:22 +0500

    it was my homework

---
+++ main.js
@@ -0,0 +1,43 @@
+const array = prompt("Ismingizni kiriting");
+const array2 = prompt("Space ID yingizni kiriting");
+const array3 = prompt("Space passwordingizni kiritng");
+
+let person = {
+    name: array,
+    id: array2,
+    password: array3,
+}
+
+console.log(person);
+
+// let computer = {
+//     protetsr: "protesr",
+//     ekran: "ekran",
+//     klaviatura: "klaviatura",
+//     windows: "windows",
+//     sichqoncha: "sichqoncha",
+//     hobby: personn = {
+//         play: "phone",
+//         read: "book",
+//     }
+// }
+// console.log(computer.personn);
+
+
+// let car = {
+//     rang: "qora",
+//     bakavoy: "bakavoy",
+// }
+
+// console.log(car);
+// car.test = "eshik";
+
+
+// let name = prompt("Ismingizni kiriting");
+// let space = prompt("Space ID yingizni kiriting");
+// let password = prompt("Space psswordingizni kiritng");
+// console.log("name");
+// console.log("space");
+// console.log("password");
+
+// const array = ["ISmingizni kriting"];
